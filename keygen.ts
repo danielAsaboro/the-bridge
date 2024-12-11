171e6ba7358993287fb1ff0a332a48664a46b960
Daniel Asaboro <asaborodaniel@gmail.com>
2024-12-11 10:23:09 +0100

    add transfer code

--- keygen.ts
+++ keygen.ts
@@ -6,10 +6,10 @@ import fs from "fs";
 let kp = Keypair.generate();
 console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}`);
 console.log(`Solana Wallet Secret Key: [${kp.secretKey}]`);
-fs.writeFileSync(
-  "dev-wallet.json",
-  JSON.stringify(Array.from(kp.secretKey), null, 2),
-  "utf-8"
-);
+// fs.writeFileSync(
+//   "dev-wallet.json",
+//   JSON.stringify(Array.from(kp.secretKey), null, 2),
+//   "utf-8"
+// );
 
-console.log("Secret key successfully saved to dev-wallet.json");
+// console.log("Secret key successfully saved to dev-wallet.json");
